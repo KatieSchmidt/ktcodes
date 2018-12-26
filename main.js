58a1918bac6168b25b0839a0458f99b6f7d7d4ea
Katie Schmidt <katie_schmidt7@icloud.com>
2018-12-26 16:00:47 -0500

    set up technology section with functionality

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 let descriptionCounter = 0;
+
 function changeDescription() {
   const developerDescriptions = ["Developer", "Creator", "Innovator"];
   let currentDescription = document.getElementById("header__text-description");
-
-  console.log(
-    `descriptionCounter: ${descriptionCounter} developerDescriptions length: ${
-      developerDescriptions.length
-    }`
-  );
 
   if (descriptionCounter < developerDescriptions.length - 1) {
     descriptionCounter += 1;
@@ -15,5 +10,25 @@ function changeDescription() {
   } else {
     descriptionCounter = 0;
     currentDescription.innerHTML = developerDescriptions[descriptionCounter];
+  }
+}
+
+let logoCounter = 0;
+
+function changeLogo() {
+  console.log("working");
+  const developerLogos = [
+    "./img/html5-css3-js.png",
+    "./img/mongodb.jpg",
+    "./img/bootstrap.png"
+  ];
+  let currentLogo = document.getElementById("about__technologies-logo");
+
+  if (logoCounter < developerLogos.length - 1) {
+    logoCounter += 1;
+    currentLogo.src = developerLogos[logoCounter];
+  } else {
+    logoCounter = 0;
+    currentLogo.src = developerLogos[logoCounter];
   }
 }
